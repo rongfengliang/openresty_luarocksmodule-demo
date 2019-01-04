90177e3ccc22cacca99e44551a0ba502dee39922
dalongrong <1141591465@qq.com>
2019-01-04 10:27:34 +0800

    add attachment demo

--- app/mail/init.lua
+++ app/mail/init.lua
@@ -17,10 +17,53 @@ function  send()
         subject = "荣锋亮测试!",
         text = "There's pizza in the sewer.",
         html = "<h1>There's pizza in the sewer.</h1>",
+        attachments = {
+          {
+            filename = "toppings.txt",
+            content_type = "text/plain",
+            content = "1. Cheese\n2. Pepperoni",
+          },
+        },
       })
       if err then
         ngx.log(ngx.ERR, "mailer:send error: ", err)
         return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
       end
 end
-return  send
+
+function  send2()
+  local mailer, err = mail.new({
+      host = "mail host",
+      port = 25,
+      ssl=false,
+      username = "<account>",
+      password = "<password>",
+    })
+    if err then
+      ngx.log(ngx.ERR, "mail.new error: ", err)
+      return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+    
+    local ok, err = mailer:send({
+      from = "dalongrong <account>",
+      to = { "1141591465@qq.com" },
+      subject = "荣锋亮测试!",
+      text = "There's pizza in the sewer.",
+      html = "<h1>There's pizza in the sewer.</h1>",
+      attachments = {
+        {
+          filename = "toppings.txt",
+          content_type = "text/plain",
+          content = "1. Cheese\n2. Pepperoni",
+        },
+      },
+    })
+    if err then
+      ngx.log(ngx.ERR, "mailer:send error: ", err)
+      return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+end
+return  {
+  send =send,
+  send2=send2
+}
